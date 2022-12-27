ba658a89cc8758d7bab547f48d9142737e552a78
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2022-12-27 18:07:52 -0300

    User login

--- minisite/src/pages/Login/Login.js
+++ minisite/src/pages/Login/Login.js
@@ -1,11 +1,72 @@
-import styles from "./Login.module.css"
+import styles from "./Login.module.css";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Login = () => {
-  return (
-    <div>
-        <h2>Login</h2>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default Login
+  const { login, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await login(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+  return (
+    <div className={styles.login}>
+      <h1>Entrar</h1>
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">
+          <span>Email:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Email do usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="">
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
