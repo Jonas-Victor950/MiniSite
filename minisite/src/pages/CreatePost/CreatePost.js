195e37b126f5a1581b77a32c9bd0126d0ed57f85
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2022-12-30 17:50:42 -0300

    data validation

--- minisite/src/pages/CreatePost/CreatePost.js
+++ minisite/src/pages/CreatePost/CreatePost.js
@@ -16,26 +16,40 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate()
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     // validate Image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL");
+    }
 
     // criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     // checar todos os valores
+    if(!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!!")
+    }
+
+    if (formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
     });
 
     // redirect to home page
+    navigate("/")
   };
   return (
     <div className={styles.create_post}>
@@ -91,7 +105,9 @@ const CreatePost = () => {
             Aguarde...
           </button>
         )}
-        {(response.error || formError) && (<p className="error">{response.error || formError}</p>)}
+        {(response.error || formError) && (
+          <p className="error">{response.error || formError}</p>
+        )}
       </form>
     </div>
   );
